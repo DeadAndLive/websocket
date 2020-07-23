91cab8cac1e74274dbf19b5b5a6c517dce793104
DeadAndLive <51323917+DeadAndLive@users.noreply.github.com>
2020-07-23 14:11:16 +0800

    Update loginout.js

--- src/assets/js/loginout.js
+++ src/assets/js/loginout.js
@@ -1,5 +1,5 @@
 let loginOutClick = $(".loginOut");
-let loginOut = new WebSocket("ws://123.57.19.35:9814/loginOut");
+let loginOut = new WebSocket("wss://123.57.19.35:9814/loginOut");
 $(loginOutClick).on("click",function() {
 	loginOut.send("out");
 });
